db3128ac252eb9e4d02e877a69f06b145da68fa0
Dolu <charles.papon.90@gmail.com>
2015-05-10 18:48:48 +0100

    SpinalHDl 1.3.5

--- build.sbt
+++ build.sbt
@@ -1,11 +1,14 @@
-name := "SpinalBaseProject"
+name := "SpinalTemplateSbt"
 
 version := "1.0"
 
-scalaVersion := "2.11.6"
+scalaVersion := "2.11.12"
 
+EclipseKeys.withSource := true
 
 libraryDependencies ++= Seq(
-  "com.github.spinalhdl" % "spinalhdl-core_2.11" % "latest.release",
-  "com.github.spinalhdl" % "spinalhdl-lib_2.11" % "latest.release"
+  "com.github.spinalhdl" % "spinalhdl-core_2.11" % "1.3.5",
+  "com.github.spinalhdl" % "spinalhdl-lib_2.11" % "1.3.5"
 )
+
+fork := true
--- src/main/scala/mylib/MyTopLevel.scala
+++ src/main/scala/mylib/MyTopLevel.scala
@@ -16,31 +16,52 @@
  * License along with this library.
  */
 
-package MyCode
+package mylib
 
 import spinal.core._
 import spinal.lib._
 
+import scala.util.Random
+
+//Hardware definition
 class MyTopLevel extends Component {
   val io = new Bundle {
-    val cond0 = in Bool
-    val cond1 = in Bool
-    val flag = out Bool
-    val state = out UInt(8 bit)
+    val cond0 = in  Bool
+    val cond1 = in  Bool
+    val flag  = out Bool
+    val state = out UInt(8 bits)
   }
-  val counter = Reg(UInt(8 bit)) init(0)
+  val counter = Reg(UInt(8 bits)) init(0)
 
   when(io.cond0){
     counter := counter + 1
   }
 
   io.state := counter
-  io.flag := (counter === 0) | io.cond1
+  io.flag  := (counter === 0) | io.cond1
 }
 
-object MyTopLevel {
+//Generate the MyTopLevel's Verilog
+object MyTopLevelVerilog {
+  def main(args: Array[String]) {
+    SpinalVerilog(new MyTopLevel)
+  }
+}
+
+//Generate the MyTopLevel's VHDL
+object MyTopLevelVhdl {
   def main(args: Array[String]) {
     SpinalVhdl(new MyTopLevel)
   }
 }
 
+
+//Define a custom SpinalHDL configuration with synchronous reset instead of the default asynchronous one. This configuration can be resued everywhere
+object MySpinalConfig extends SpinalConfig(defaultConfigForClockDomains = ClockDomainConfig(resetKind = SYNC))
+
+//Generate the MyTopLevel's Verilog using the above custom configuration.
+object MyTopLevelVerilogWithCustomConfig {
+  def main(args: Array[String]) {
+    MySpinalConfig.generateVerilog(new MyTopLevel)
+  }
+}
