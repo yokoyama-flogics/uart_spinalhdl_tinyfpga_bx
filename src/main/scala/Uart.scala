52fb84ec37a86664a070c86c2a7e8a0393d1e7ac
Atsushi Yokoyama <yokoyama@flogics.com>
2020-01-28 14:33:53 +0900

    First version of UartCore

--- src/main/scala/Uart.scala
+++ src/main/scala/Uart.scala
@@ -1,67 +1,84 @@
-/*
- * SpinalHDL
- * Copyright (c) Dolu, All rights reserved.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3.0 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library.
- */
-
-package mylib
-
 import spinal.core._
-import spinal.lib._
+import spinal.lib.fsm._
 
-import scala.util.Random
-
-//Hardware definition
-class MyTopLevel extends Component {
+class UartCore(len_data: Int) extends Component {
   val io = new Bundle {
-    val cond0 = in  Bool
-    val cond1 = in  Bool
-    val flag  = out Bool
-    val state = out UInt(8 bits)
-  }
-  val counter = Reg(UInt(8 bits)) init(0)
-
-  when(io.cond0){
-    counter := counter + 1
+    val valid = in Bool
+    val ready = out Bool
+    val payload = in Bits (len_data bits)
+    val txd = out Bool
   }
 
-  io.state := counter
-  io.flag  := (counter === 0) | io.cond1
+  val period_timer = 16 * 1000 * 1000 / 115200
+
+  val n_bits_sent = Reg(UInt(log2Up(len_data) bits)) // # bits already sent
+  // val ct_timer = Reg(UInt((16 * 1000 * 1000 / 115200) bits))
+  val ct_timer = Reg(UInt(log2Up(period_timer) bits))
+  val data = Reg(Bits(len_data bits))
+
+  io.ready := False
+  io.txd := True
+
+  val fsm = new StateMachine {
+    val idle = new State with EntryPoint
+    val startbit = new State
+    val sending = new State
+    val stopbit = new State
+
+    idle
+      .whenIsActive {
+        io.ready := True
+        io.txd := True
+        when(io.valid) {
+          n_bits_sent := 0
+          ct_timer := 0
+          data := io.payload
+          goto(startbit)
+        }
+      }
+
+    startbit
+      .whenIsActive {
+        ct_timer := ct_timer + 1
+        io.ready := False
+        io.txd := False
+        when(ct_timer === period_timer - 1) {
+          ct_timer := 0
+          goto(sending)
+        }
+      }
+
+    sending
+      .whenIsActive {
+        ct_timer := ct_timer + 1
+        io.ready := False
+        io.txd := data(0)
+        when(ct_timer === period_timer - 1) {
+          ct_timer := 0
+          data := (data >> 1).resized
+          n_bits_sent := n_bits_sent + 1
+          when(n_bits_sent === len_data - 1) {
+            n_bits_sent := 0
+            goto(stopbit)
+          }
+        }
+      }
+
+    stopbit
+      .whenIsActive {
+        ct_timer := ct_timer + 1
+        io.ready := False
+        io.txd := True
+        when(ct_timer === period_timer - 1) {
+          ct_timer := 0
+          goto(idle)
+        }
+      }
+  }
 }
 
-//Generate the MyTopLevel's Verilog
-object MyTopLevelVerilog {
-  def main(args: Array[String]) {
-    SpinalVerilog(new MyTopLevel)
-  }
-}
-
-//Generate the MyTopLevel's VHDL
-object MyTopLevelVhdl {
-  def main(args: Array[String]) {
-    SpinalVhdl(new MyTopLevel)
-  }
-}
-
-
-//Define a custom SpinalHDL configuration with synchronous reset instead of the default asynchronous one. This configuration can be resued everywhere
-object MySpinalConfig extends SpinalConfig(defaultConfigForClockDomains = ClockDomainConfig(resetKind = SYNC))
-
-//Generate the MyTopLevel's Verilog using the above custom configuration.
-object MyTopLevelVerilogWithCustomConfig {
-  def main(args: Array[String]) {
-    MySpinalConfig.generateVerilog(new MyTopLevel)
+object UartCoreVerilog {
+  def main(args: Array[String]): Unit = {
+    SpinalVerilog(new UartCore(len_data = 8))
   }
 }
---
+++ src/main/scala/UartSim.scala
@@ -0,0 +1,26 @@
+import spinal.core._
+import spinal.sim._
+import spinal.core.sim._
+
+object UartCoreSim {
+  def main(args: Array[String]): Unit = {
+    SimConfig.withWave.doSim(new UartCore(len_data = 8)) { dut =>
+      //Fork a process to generate the reset and the clock on the dut
+      dut.clockDomain.forkStimulus(period = 10)
+
+      dut.io.payload #= 0
+      dut.io.valid #= false
+
+      for (idx <- 0 to 5000) {
+        if (idx == 10) {
+          dut.io.payload #= 123
+          dut.io.valid #= true
+        } else if (idx == 15) {
+          dut.io.payload #= 0
+          dut.io.valid #= false
+        }
+        dut.clockDomain.waitSampling()
+      }
+    }
+  }
+}
