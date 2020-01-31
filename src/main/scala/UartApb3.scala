37bfa9585caa9ed8741c54ef7006bcccf0393462
Atsushi Yokoyama <yokoyama@flogics.com>
2020-01-31 15:53:45 +0900

    Implemented UartApb3 (register-mapped UART) by hand

---
+++ src/main/scala/UartApb3.scala
@@ -0,0 +1,62 @@
+import spinal.core._
+
+class UartApb3(
+    len_data: Int,
+    clock_rate: HertzNumber,
+    bit_rate: HertzNumber
+) extends Component {
+  val io = new Bundle {
+    val PADDR = in UInt (32 bits)
+    val PSEL = in Bool
+    val PENABLE = in Bool
+    val PREADY = out Bool
+    val PWRITE = in Bool
+    val PWDATA = in Bits (32 bits)
+    val PRDATA = out Bits (32 bits)
+    val PSLVERROR = out Bool
+    val txd = out Bool
+  }
+
+  /*
+   * Register Address Definitions
+   */
+  val addr_base = 0x20000000
+  val addr_txd = addr_base + 0
+  val addr_status = addr_base + 4 // bit 0: tx_ready
+
+  /*
+   * Instantiation of a UART Core
+   */
+  val uart = new UartCore(
+    len_data = 8,
+    clock_rate = clock_rate,
+    bit_rate = bit_rate
+  )
+  uart.io.txd <> io.txd
+
+  /*
+   * Write and Read Conditions
+   */
+  val write: Bool = io.PSEL && io.PENABLE && io.PWRITE
+  val read: Bool = io.PSEL && io.PENABLE && !io.PWRITE
+
+
+  /*
+   * Main Part
+   */
+  io.PREADY := False
+  io.PRDATA := 0
+  io.PSLVERROR := False
+
+  uart.io.valid := False
+  uart.io.payload := 0
+
+  when(io.PADDR === addr_txd && write) {
+    io.PREADY := True
+    uart.io.valid := True
+    uart.io.payload := io.PWDATA.resized
+  }.elsewhen(io.PADDR === addr_status && read) {
+    io.PREADY := True
+    io.PRDATA := uart.io.ready.asBits(1 bits).resized
+  }
+}
--- src/main/scala/UartSim.scala
+++ src/main/scala/UartSim.scala
@@ -49,3 +49,68 @@ object UartTxStringSim {
     }
   }
 }
+
+object UartApb3Sim {
+  def main(args: Array[String]): Unit = {
+    SimConfig.withWave.doSim(
+      new UartApb3(
+        len_data = 8,
+        clock_rate = 16 MHz,
+        bit_rate = 115200 Hz
+      )
+    ) { dut =>
+      //Fork a process to generate the reset and the clock on the dut
+      dut.clockDomain.forkStimulus(period = 10)
+
+      dut.io.PADDR #= 0
+      dut.io.PSEL #= false
+      dut.io.PENABLE #= false
+      dut.io.PWRITE #= false
+      dut.io.PWDATA #= 0
+
+      for (idx <- 0 to 5000) {
+        /*
+         * Writing to UartApb3 TxD register
+         */
+        if (idx == 10) {
+          dut.io.PADDR #= 0x20000000
+          dut.io.PSEL #= true
+          dut.io.PWRITE #= true
+          dut.io.PWDATA #= 0x5a
+        } else if (idx == 11) {
+          dut.io.PENABLE #= true
+        } else if (idx == 12) {
+          dut.io.PSEL #= false
+          dut.io.PENABLE #= false
+
+        /*
+         * Reading from UartApb3 status register (expecting to read 0)
+         */
+        } else if (idx == 20) {
+          dut.io.PADDR #= 0x20000004
+          dut.io.PSEL #= true
+          dut.io.PWRITE #= false
+        } else if (idx == 21) {
+          dut.io.PENABLE #= true
+        } else if (idx == 22) {
+          dut.io.PSEL #= false
+          dut.io.PENABLE #= false
+
+        /*
+         * Reading from UartApb3 status register (expecting to read 1)
+         */
+        } else if (idx == 1620) {
+          dut.io.PADDR #= 0x20000004
+          dut.io.PSEL #= true
+          dut.io.PWRITE #= false
+        } else if (idx == 1621) {
+          dut.io.PENABLE #= true
+        } else if (idx == 1622) {
+          dut.io.PSEL #= false
+          dut.io.PENABLE #= false
+        }
+        dut.clockDomain.waitSampling()
+      }
+    }
+  }
+}
